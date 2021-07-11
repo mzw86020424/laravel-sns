947c43b3f5a2eded882beb2c79c692de99265fe9
mzw86020424 <mzw86020424@gmail.com>
2021-07-11 15:10:34 +0900

    記事更新画面と記事削除モーダル画面の表示

--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div class="md-form">
   <label for="">title</label>
-  <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+  <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="body" required rows="16" class="form-control" placeholder="本文">{{ old('body') }}</textarea>
+  <textarea name="body" required rows="16" class="form-control" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
