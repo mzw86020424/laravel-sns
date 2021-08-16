47379086d4d760cbeac4f489f5c43cf99e435cd1
mzw86020424 <mzw86020424@gmail.com>
2021-08-16 12:07:20 +0900

    VueコンポーネントをBladeに組み込む

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -14,9 +14,11 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
 </head>
 <body>
+  <div id="app">
+    @yield('content')
+  </div>
 
-  @yield('content')
-
+  <script src="{{ mix('js/app.js') }}"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
