69819027c369af4b1b2bed861e2796aa38b16f72
mzw86020424 <mzw86020424@gmail.com>
2021-08-19 09:25:36 +0900

    いいね数を算出してBladeを通じてVueに渡す

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <button type="button" class="btn m-0 p-1 shadow-none">
-      <i class="fas fa-heart mr-1" :class="{'red-text':this.isLikedBy}"/>
+    <button
+      type="button"
+      class="btn m-0 p-1 shadow-none"
+    >
+      <i
+        class="fas fa-heart mr-1"
+        :class="{'red-text':this.isLikedBy}"
+      />
     </button>
-    10
+    {{ countLikes }}
   </div>
 </template>
 
@@ -14,10 +20,15 @@
         type: Boolean,
         default: false,
       },
+      initialCountLikes: {
+        type: Number,
+        default: 0,
+      }
     },
     data() {
       return {
         isLikedBy: this.initialIsLikedBy,
+        countLikes: this.initialCountLikes,
       }
     },
   }
