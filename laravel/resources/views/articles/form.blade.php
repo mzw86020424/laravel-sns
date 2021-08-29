7285f4266a4567ebbb3417dd2a827209374801c9
mzw86020424 <mzw86020424@gmail.com>
2021-08-29 11:25:52 +0900

    タグ入力のVueコンポーネントを表示する

--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -4,6 +4,11 @@
   <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
+  <article-tags-input>
+    
+  </article-tags-input>
+</div>
+<div class="form-group">
   <label></label>
   <textarea name="body" required rows="16" class="form-control" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
