dba912fbf98e5f6a22dd1171162ab70a85e0060f
mzw86020424 <mzw86020424@gmail.com>
2021-10-02 15:44:19 +0900

    9-3 フォローボタンを表示する

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -12,7 +12,13 @@
             <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
               <i class="fas fa-user-circle fa-3x"></i>
             </a>
+            @if (Auth::id() !== $user->id)
+            <follow-button
+            class="ml-auto"
+            @endif
+            >
           </div>
+
           <h2 class="h5 card-title m-0">
             <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
             {{ $user->name }}
